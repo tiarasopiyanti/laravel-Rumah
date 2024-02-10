29119775e3c503e24a983ebb8b56b4a6b8577fcb
tiarasopiyanti <118708571+tiarasopiyanti@users.noreply.github.com>
2024-02-10 08:25:09 -0800

    Add files via upload

---
+++ 2024_02_10_160234_create_pembayarans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pembayarans', function (Blueprint $table) {
+            $table->id('id_pembayaran');
+            $table->foreignId('id_petugas')->references('id_petugas')->on('petugases');
+            $table->char('nisn', 10);
+            $table->foreign('nisn')->references('nisn')->on('siswas');
+            $table->date('tgl_bayar');
+            $table->string('bulan_dibayar', 8);
+            $table->string('tahun_dibayar', 4);
+            $table->foreignId('id_spp')->references('id_spp')->on('siswas');
+            $table->integer('jumlah_bayar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pembayarans');
+    }
+};
